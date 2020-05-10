d0b8703bcb7b62d5ea5605ac52a8446b9d5b38a0
Spijavca Veaceslav <dreamersf@gmail.com>
2020-05-10 22:02:49 +0300

    Mult and Div

--- ls3.py
+++ ls3.py
@@ -1,2 +1,33 @@
-print('Hi')
-a = 10
+print('Введите два положительных числа')
+
+while True:
+    try:
+        print("Введите число А")
+        a = int(input())
+        if a > 0 or a == 0:
+            print("Номер А подходит")
+            break
+        else:
+            print("Введите не отрицательное число А")
+    except ValueError:
+        print("Вы ввели не число!")
+
+while True:
+    try:
+        print("Введите число Б")
+        b = int(input())
+        if b > 0 or b == 0:
+            print("Номер Б подходит")
+            break
+        else:
+            print("Введите не отрицательное число Б")
+    except ValueError:
+        print("Вы ввели не число!")
+
+try:
+    mul = a * b
+    print("mul", mul)
+    div = a / b
+    print("div", div)
+except ZeroDivisionError:
+        print("Деление на 0 невозможно, укажите другое число Б")
